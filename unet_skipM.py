ba80c4befc6fbe2bbf5d9e9c64eb70ed0f288077
zhongyuan1996 <33609452+zhongyuan1996@users.noreply.github.com>
2025-03-26 15:42:18 -0400

    Update unet_skipM.py
    moved attention and maxpool layer to this file

--- unet_skipM.py
+++ unet_skipM.py
@@ -1,7 +1,76 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from models.baseline import Attention, MaxPoolLayer
+
+class MaxPoolLayer(nn.Module):
+    """
+    A layer that performs max pooling along the sequence dimension
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, inputs, mask_or_lengths=None):
+        """
+        inputs: tensor of shape (batch_size, seq_len, hidden_size)
+        mask_or_lengths: tensor of shape (batch_size) or (batch_size, seq_len)
+
+        returns: tensor of shape (batch_size, hidden_size)
+        """
+        bs, sl, _ = inputs.size()
+        if mask_or_lengths is not None:
+            if len(mask_or_lengths.size()) == 1:
+                mask = (torch.arange(sl, device=inputs.device).unsqueeze(0).expand(bs, sl) >= mask_or_lengths.unsqueeze(
+                    1))
+            else:
+                mask = mask_or_lengths
+            inputs = inputs.masked_fill(mask.unsqueeze(-1).expand_as(inputs), float('-inf'))
+        max_pooled = inputs.max(1)[0]
+        return max_pooled
+
+class Attention(nn.Module):
+    def __init__(self, in_feature, num_head, dropout):
+        super(Attention, self).__init__()
+        self.in_feature = in_feature
+        self.num_head = num_head
+        self.size_per_head = in_feature // num_head
+        self.out_dim = num_head * self.size_per_head
+        assert self.size_per_head * num_head == in_feature
+        self.q_linear = nn.Linear(in_feature, in_feature, bias=False)
+        self.k_linear = nn.Linear(in_feature, in_feature, bias=False)
+        self.v_linear = nn.Linear(in_feature, in_feature, bias=False)
+        self.fc = nn.Linear(in_feature, in_feature, bias=False)
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(in_feature)
+
+    def forward(self, query, key, value, attn_mask=None):
+        batch_size = key.size(0)
+        res = query
+        query = self.q_linear(query)
+        key = self.k_linear(key)
+        value = self.v_linear(value)
+
+        query = query.view(batch_size, self.num_head, -1, self.size_per_head)
+        key = key.view(batch_size, self.num_head, -1, self.size_per_head)
+        value = value.view(batch_size, self.num_head, -1, self.size_per_head)
+
+        scale = np.sqrt(self.size_per_head)
+        energy = torch.matmul(query, key.permute(0, 1, 3, 2)) / scale
+        if attn_mask is not None:
+            batch_size, q_len, k_len = attn_mask.size()
+            attn_mask = attn_mask.unsqueeze(1).expand(batch_size, self.num_head, q_len, k_len)
+            energy = energy.masked_fill(attn_mask == 0, -np.inf)
+
+        attention = torch.softmax(energy, dim=-1)
+        x = torch.matmul(attention, value)
+        x = x.permute(0, 2, 1, 3).contiguous()
+        x = x.view(batch_size, -1, self.in_feature)
+        attention = attention.sum(dim=1).squeeze().permute(0, 2, 1) / self.num_head
+        x = self.fc(x)
+        x = self.dropout(x)
+        x += res
+        x = self.layer_norm(x)
+        return x, attention
 
 class timegap_predictor(nn.Module):
     def __init__(self, d_model):
